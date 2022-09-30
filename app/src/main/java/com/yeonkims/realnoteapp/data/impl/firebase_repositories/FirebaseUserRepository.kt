6a9dc37fadbd060409a976434b293e08cde7a08c
Jiyeon Kim <jiyeon7269@naver.com>
2022-09-30 02:42:19 +0100

    feat: login/signup using Firebase

--- app/src/main/java/com/yeonkims/realnoteapp/data/impl/firebase_repositories/FirebaseUserRepository.kt
+++ app/src/main/java/com/yeonkims/realnoteapp/data/impl/firebase_repositories/FirebaseUserRepository.kt
@@ -2,28 +2,48 @@ package com.yeonkims.realnoteapp.data.impl.firebase_repositories
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.gson.Gson
+import com.yeonkims.realnoteapp.data.models.Note
 import com.yeonkims.realnoteapp.data.models.User
 import com.yeonkims.realnoteapp.data.repositories.UserRepository
 import com.yeonkims.realnoteapp.util.aliases.BooleanFunction
+import java.util.HashMap
 import javax.inject.Inject
 
 class FirebaseUserRepository @Inject constructor(
-    private val auth: FirebaseAuth
+    private val auth: FirebaseAuth,
+    private val functions: FirebaseFunctions,
+    private val gson: Gson
 ) : UserRepository {
+
+    private val currentUser = MutableLiveData<User>(null)
+
+    override fun getCurrentUser(): LiveData<User> {
+        return currentUser
+    }
 
     override suspend fun login(email: String, password: String, onCompleteListener: BooleanFunction) {
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if(task.isSuccessful) {
-                val result = task.result
-                Log.i(javaClass.simpleName, "SUCCESS ${result.user?.uid}")
+            if (task.isSuccessful) {
+                val id = task.result.user?.uid
+
+                functions.getHttpsCallable("getUser").call(mapOf("id" to id))
+                    .addOnCompleteListener { response ->
+                        val data = response.result.data
+                        val jsonData = (data as HashMap<*, *>)["res"]
+                        val dbSavedUser = gson.fromJson(
+                            gson.toJson(jsonData),
+                            Array<User>::class.java
+                        ).first()
+                        currentUser.value = dbSavedUser
+                        onCompleteListener(response.isSuccessful)
+                    }
+            } else {
+                onCompleteListener(task.isSuccessful)
             }
-            else {
-                task.exception?.message
-                Log.i(javaClass.simpleName, "FAILED ${task.exception?.javaClass?.simpleName}")
-                Log.i(javaClass.simpleName, task.exception?.message.toString())
-            }
-            onCompleteListener(task.isSuccessful)
         }
     }
 
@@ -34,15 +54,17 @@ class FirebaseUserRepository @Inject constructor(
     ) {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if(task.isSuccessful) {
-                val result = task.result
-                Log.i(javaClass.simpleName, "SUCCESS ${result.user?.uid}")
+                val id = task.result.user?.uid
+                functions.getHttpsCallable("createUser").call(mapOf("id" to id, "email" to email))
+                    .addOnCompleteListener { response ->
+                        if(response.isSuccessful) {
+                            currentUser.value = User(id!!, email)
+                        }
+                        onCompleteListener(response.isSuccessful)
+                    }
+            } else {
+                onCompleteListener(task.isSuccessful)
             }
-            else {
-                task.exception?.message
-                Log.i(javaClass.simpleName, "FAILED ${task.exception?.javaClass?.simpleName}")
-                Log.i(javaClass.simpleName, task.exception?.message.toString())
-            }
-            onCompleteListener(task.isSuccessful)
         }
     }
 
@@ -52,10 +74,6 @@ class FirebaseUserRepository @Inject constructor(
 
 
     override suspend fun logout() {
-        TODO("Not yet implemented")
-    }
-
-    override fun getCurrentUser(): LiveData<User> {
-        TODO("Not yet implemented")
+        // currentUser.value = null
     }
 }
