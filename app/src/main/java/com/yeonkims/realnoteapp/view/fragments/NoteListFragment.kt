d5c7ae347c5a52d2f95c30eb307fe5b1f7d34885
Jiyeon Kim <jiyeon7269@naver.com>
2022-11-02 00:46:08 +0000

    feat: add navigation drawer + toolbar menu

--- app/src/main/java/com/yeonkims/realnoteapp/view/fragments/NoteListFragment.kt
+++ app/src/main/java/com/yeonkims/realnoteapp/view/fragments/NoteListFragment.kt
@@ -1,46 +1,93 @@
 package com.yeonkims.realnoteapp.view.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.yeonkims.realnoteapp.R
 import com.yeonkims.realnoteapp.databinding.FragmentNoteListBinding
 import com.yeonkims.realnoteapp.logic.viewmodels.auth.AuthViewModel
 import com.yeonkims.realnoteapp.logic.viewmodels.note.NotesViewModel
+import com.yeonkims.realnoteapp.util.dev_tools.Logger
 import com.yeonkims.realnoteapp.view.dialogs.LogoutDialog
 import com.yeonkims.realnoteapp.view.recyclerViewAdapter.NoteListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class NoteListFragment : Fragment() {
 
     @Inject
     lateinit var viewModel: NotesViewModel
+
     @Inject
     lateinit var authViewModel: AuthViewModel
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding : FragmentNoteListBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_note_list, container, false)
+        val binding: FragmentNoteListBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_note_list, container, false
+        )
 
         val adapter = NoteListAdapter(viewModel)
 
-        val activity  = requireActivity()
-        activity.setActionBar(binding.toolbar)
-        binding.toolbar.inflateMenu(R.menu.note_list_menu)
+        val activity = requireActivity()
+        val navController = findNavController()
+
+        val navigationView = binding.navView
+        val fragmentManger = parentFragmentManager
+
+        drawerLayout = binding.drawerLayout
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            activity, drawerLayout, binding.toolbar, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        binding.toolbar.title = "Real Notes"
+        binding.toolbar.inflateMenu(R.menu.create_note_menu)
+        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.create_menu -> {
+                    val action = NoteListFragmentDirections
+                        .actionNoteListFragmentToSelectedNoteFragment(null)
+                    navController.navigate(action)
+                }
+            }
+            true
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.logout_menu -> {
+                    LogoutDialog().show(
+                        fragmentManger, LogoutDialog.TAG
+                    )
+                }
+            }
+            true
+        }
 
         viewModel.currentUser.observe(viewLifecycleOwner) { currentUser ->
-            if(currentUser == null) {
+            if (currentUser == null) {
                 Log.i(javaClass.simpleName, javaClass.simpleName)
                 val action = NoteListFragmentDirections.actionNoteListFragmentToLoginFragment()
                 findNavController().navigate(action)
@@ -57,38 +104,4 @@ class NoteListFragment : Fragment() {
 
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        val menuHost: MenuHost = requireActivity()
-        val navController = findNavController()
-
-        val fragmentManger = parentFragmentManager
-
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.note_list_menu, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                when (menuItem.itemId) {
-                    R.id.create_menu -> {
-                        val action = NoteListFragmentDirections
-                            .actionNoteListFragmentToSelectedNoteFragment(null)
-                        navController.navigate(action)
-
-                    }
-                    R.id.logout_menu -> {
-                        LogoutDialog().show(
-                            fragmentManger, LogoutDialog.TAG
-                        )
-                    }
-                }
-                return true
-            }
-
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-
-    }
-
 }
